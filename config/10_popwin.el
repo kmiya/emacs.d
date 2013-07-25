62d7d6cd0344daa68f2e0b579dabbfcaee553076
kmiya <kmiya@outlook.com>
2013-07-25 19:54:54 +0900

    add perl settings

--- config/10_popwin.el
+++ config/10_popwin.el
@@ -1,9 +1,8 @@
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
 
-(setq anything-samewindow nil)
 (push '("*anything*" :height 20) popwin:special-display-config)
-(push '("*helm*" :height 20) popwin:special-display-config)
 (push '(dired-mode :position top) popwin:special-display-config)
 (push '("*auto-async-byte-compile*" :height 20) popwin:special-display-config)
 (push '("*Compile-Log*" :height 20) popwin:special-display-config)
+(push '("^\*helm .+\*$" :regexp t) popwin:special-display-config)
--- config/60_perl.el
+++ config/60_perl.el
@@ -1,2 +1,82 @@
-(dolist (hook '(perl-mode-hook))
+(require 'perltidy)
+
+(autoload 'cperl-mode "cperl-mode" "alternate mode for editing Perl programs" t)
+(add-to-list 'auto-mode-alist '("\.\([pP][Llm]\|al\|t\|cgi\)\'" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
+;;; cperl-mode is preferred to perl-mode
+;;; "Brevity is the soul of wit" <foo at acm.org>
+(defalias 'perl-mode 'cperl-mode)
+
+(dolist (hook '(cperl-mode-hook))
   (add-hook hook (lambda () (flyspell-mode 1))))
+
+(setq cperl-indent-level 4
+      cperl-continued-statement-offset 4
+      cperl-close-paren-offset -4
+      cperl-label-offset -4
+      cperl-comment-column 40
+      cperl-highlight-variables-indiscriminately t
+      cperl-indent-parens-as-block t
+      cperl-tab-always-indent nil
+      cperl-font-lock t)
+(add-hook 'cperl-mode-hook
+          '(lambda ()
+             (progn
+               (setq indent-tabs-mode nil)
+               (setq tab-width nil)
+
+               ; perl-completion
+               (require 'auto-complete)
+               (require 'perl-completion)
+               (add-to-list 'ac-sources 'ac-source-perl-completion)
+               (perl-completion-mode t)
+              )))
+
+; perl tidy
+; sudo aptitude install perltidy
+(defun perltidy-region ()
+  "Run perltidy on the current region."
+  (interactive)
+  (save-excursion
+    (shell-command-on-region (point) (mark) "perltidy -q" nil t)))
+(defun perltidy-defun ()
+  "Run perltidy on the current defun."
+  (interactive)
+  (save-excursion (mark-defun)
+                  (perltidy-region)))
+
+;; flymake for perl
+(defvar flymake-perl-err-line-patterns '(("\(.*\) at \([^ n]+\) line \([0-9]+\)[,.n]" 2 3 nil 1)))
+(defconst flymake-allowed-perl-file-name-masks '(("\.pl$" flymake-perl-init)
+                                               ("\.pm$" flymake-perl-init)
+                                               ("\.t$" flymake-perl-init)
+                                               ))
+
+(defun flymake-perl-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "perl" (list "-wc" local-file))))
+
+(defun flymake-perl-load ()
+  (interactive)
+  (set-perl5lib)
+  (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
+    (setq flymake-check-was-interrupted t))
+  (ad-activate 'flymake-post-syntax-check)
+  (setq flymake-allowed-file-name-masks (append flymake-allowed-file-name-masks flymake-allowed-perl-file-name-masks))
+  (setq flymake-err-line-patterns flymake-perl-err-line-patterns)
+  (flymake-mode t))
+
+(add-hook 'cperl-mode-hook '(lambda () (flymake-perl-load)))
+
+(defun next-flymake-error ()
+  (interactive)
+  (flymake-goto-next-error)
+  (let ((err (get-char-property (point) 'help-echo)))
+    (when err
+      (message err))))
